23bdd25d8e5c91aff755aee5099b4eb4136258aa
Théotix <theotix@tux-fh.net>
2014-09-14 16:24:11 +0200

    Loading webfonts with javascript

--- template/js/script.js
+++ template/js/script.js
@@ -245,3 +245,17 @@ $(document).ready(function () {
 loadpost();
 post_extras();
 showcomment();
+
+// WebFont
+  WebFontConfig = {
+    google: { families: [ 'Open+Sans:400,700:latin', 'Libre+Baskerville:400,700:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
