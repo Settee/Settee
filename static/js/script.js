384385d15fb19ce18c24c6ecfb578a1ab24839c0
Frozennide <contact@netart.fr.nf>
2014-09-18 17:39:01 +0200

    update css

--- static/js/script.js
+++ static/js/script.js
@@ -262,6 +262,21 @@ $(document).ready(function () {
     $('.postfooterleft input[type=file]').bind('change focus click', VIGET.fileInputs);
 });
 
+
+// WebFont
+WebFontConfig = {
+        google: { families: [ 'Open+Sans:400,700:latin', 'Libre+Baskerville:400,700:latin' ] }
+    };
+    (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+})();
+
+
 // Functions start
 loadpost();
 post_extras();
